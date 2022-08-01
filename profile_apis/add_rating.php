ebca195b5e5704a08e49bc4c21c33a2bd2c28d1c
vishkalsi <vishkalsi12@gmail.com>
2022-08-01 15:59:27 -0400

    rating api added

---
+++ profile_apis/add_rating.php
@@ -0,0 +1,31 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    header('Content-Type: application/json');
+    $content = trim(file_get_contents("php://input"));
+    $decoded = json_decode($content, true);
+    if (!is_array($decoded)) {
+        header("HTTP/1.1 400 Bad Request");
+        $response = array('response' => 'error', 'responseCode' => 400, 'errorMessage' => 'Bad Request');
+        echo json_encode($response);
+    } else {
+        if (isset($decoded['userId'])) {
+            require '../dbconn.php';
+            $userId = $row['userId'];
+            $myUserId = $row['myUserId'];
+            $rating = (int) $row['rating'];
+            $review = mysqli_real_escape_string($connection,$row['review']);
+            $query = "insert into user_ratings (rating_by_user_id, user_id, review, rating) values ('$userId','$myUserId','$review','$rating')";
+            mysqli_query($connection, $query) or die('Error : ' . mysqli_error($connection));
+            $response = array('response' => 'success', 'responseCode' => 200);
+            echo json_encode($response);
+            mysqli_close($connection);
+        } else {
+            $response = array('response' => 'error', 'responseCode' => 406, 'errorMessage' => 'Wrong arguments');
+            echo json_encode($response);
+        }
+    }
+} else {
+    header("HTTP/1.1 401 Unauthorized");
+    $response = array('response' => 'error', 'responseCode' => 401, 'errorMessage' => 'Unauthorized User');
+    echo json_encode($response);
+}
