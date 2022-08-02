58ea98a4145177c4bab261f1a3c93625814ca86e
vishkalsi <vishkalsi12@gmail.com>
2022-08-02 16:27:09 -0400

    BUGS FIXED

--- profile_apis/edit-profile.php
+++ profile_apis/edit-profile.php
@@ -14,9 +14,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $userGender = mysqli_real_escape_string($connection,$decoded['userGender']) ;
             $userDOB = mysqli_real_escape_string($connection,$decoded['userDOB']);
             $userAbout =mysqli_real_escape_string($connection, $decoded['userAbout']);
+            $userDP =mysqli_real_escape_string($connection, $decoded['userDP']);
             $userId = mysqli_real_escape_string($connection, $decoded['myUserId']);
             $userFullName = mysqli_real_escape_string($connection, $decoded['userFullName']);
-            $query = "update users set user_address = '$userAddress',user_gender = '$userGender',user_dob = '$userDOB',user_about = '$userAbout',user_full_name = '$userFullName' where primary_id = $userId";
+            $query = "update users set user_address = '$userAddress',user_gender = '$userGender',user_dob = '$userDOB',user_about = '$userAbout',user_full_name = '$userFullName',user_display_picture = '$userDP' where primary_id = $userId";
             mysqli_query($connection, $query) or die('Error : ' . mysqli_error($connection));
             $response = array('response' => 'success', 'responseCode' => 200);
             echo json_encode($response);
